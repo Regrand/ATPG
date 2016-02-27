e8a3073fa394ceebb2e09b68cdf83ac9b3271758
goenkasneha <sneha1234>
2016-02-28 00:18:49 +0530

    Added node class

---
+++ ATPG/src/PODEM/node.java
@@ -0,0 +1,14 @@
+package PODEM;
+import java.util.*;
+
+
+/**
+ * Created by Sneha on 28-02-2016.
+ */
+public class node {
+    int num;
+    int value;
+    String gate;
+    ArrayList input;
+    ArrayList output;
+}
